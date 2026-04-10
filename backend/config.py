592b7f8f77c847e1b36bdd3427ed0a979c41333d
Hingaz04 <hhinga04@gmail.com>
2026-04-10 14:39:39 +0300

    Routes fixed

--- backend/config.py
+++ backend/config.py
@@ -1,18 +1,14 @@
-# config.py
 from datetime import timedelta
 from decouple import config
 
 class Config:
     SECRET_KEY = config("SECRET_KEY", default="super-secret-key")
-    SQLALCHEMY_TRACK_MODIFICATIONS = config(
-        "SQLALCHEMY_TRACK_MODIFICATIONS", default=False, cast=bool
-    )
 
-    # Use environment variable for database URL
-    SQLALCHEMY_DATABASE_URI = config(
-        "DATABASE_URL"
-    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_DATABASE_URI = config("DATABASE_URL")
 
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=2)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=1)
-    DEBUG = config("DEBUG", default=True, cast=bool)
+
+    DEBUG = config("DEBUG", default=False, cast=bool)
